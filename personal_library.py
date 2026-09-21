cef17e962e3f5e23094ce47865ca6f8ad1d8ecec
damarisL712 <dklubin@hotmail.com>
2026-09-20 23:25:12 -0400

    (code) function to remove books from the library

--- personal_library.py
+++ personal_library.py
@@ -35,12 +35,12 @@ def display_menu():
         none
     '''
     print("\n==== Personal Library Manager ====")
-    print("1. Please select an option:")
-    print("2. Add a title")
-    print("3. Remove a book")
-    print("4. List all book titles")
-    print("5. Search a book title")
-    print("6. Exit")
+    print(" Please select an option:")
+    print("1. Add a title")
+    print("2. Remove a book")
+    print("3. List all book titles")
+    print("4. Search a book title")
+    print("5. Exit")
 
 def add_book(library):
     '''
@@ -49,7 +49,7 @@ def add_book(library):
     parameters: 
         Library: A list containing book titles
 
-    Returns:
+    returns:
         none 
     '''
     title = input("Enter a book title: ")
@@ -77,7 +77,7 @@ def list_books(library):
         print("\n==== Your Library ====")
         for index in range(len(library)):
             # print the book number, then print the book title at the position
-            print(f"{index +1}. {library[index]}")
+            print(f"{index + 1 }. {library[index]}")
 
 def remove_books(library):
     '''
@@ -88,13 +88,24 @@ def remove_books(library):
 
     returns:
     '''
-    # commit test
-    #commit test2 
+    title = input("Enter the book title you want to remove: ")
+
+    if title in library:
+        library.remove(title)
+        print(f"{title} was remove from your library")
+    else:
+        print(f"{title} was not found in your library. Try again :(")
+
 def main():
     library = []
     display_menu()
-    add_book(library)
-    list_books(library)
+    choice = input("Choose an option from the list:")
+    if choice == "1":
+        add_book(library)
+    elif choice == "2":
+        lists_books(library)
+    elif choice == "3":
+        remove_books(library)
 
 if __name__ == "__main__":
     main()
